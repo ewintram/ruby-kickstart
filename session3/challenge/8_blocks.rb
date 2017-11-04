bd4f59f38630398ea2df6af8a7ac908e76b71d27
Eleanor Wintram <eleanorwintram@gmail.com>
2017-11-04 11:46:32 +0000

    Session3 7-8

--- session3/challenge/8_blocks.rb
+++ session3/challenge/8_blocks.rb
@@ -26,14 +26,17 @@
 
 
 class Person
-  attr_accessor :name
+  attr_accessor :name, :age, :quote
 
-  def initialize(&initializer)
-    @initializer = initializer
-    initializer.call self
+  def initialize(options=Hash.new, &initializer)
+    @name, @age, @quote = options[:name], options[:age], options[:quote]
+    @initializer = (initializer || Proc.new { |person| })
+    reinit
   end
 
   def reinit
     @initializer.call self
   end
+
+
 end
